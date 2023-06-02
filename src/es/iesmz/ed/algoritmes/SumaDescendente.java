a7d7b628080ba9b98e36bf14a5a4d586392ae6ef
Alberto Nieto <anietor23@gmail.com>
2023-06-02 17:47:53 +0200

    Documentación

--- src/es/iesmz/ed/algoritmes/SumaDescendente.java
+++ src/es/iesmz/ed/algoritmes/SumaDescendente.java
@@ -1,11 +1,27 @@
 package es.iesmz.ed.algoritmes;
 
+/**
+ * Esta clase calcula la suma descendente de un numero.
+ * @author: Alberto Nieto
+ * @version: 02/06/2023
+ */
+
 public class SumaDescendente {
     private long numero;
 
+    /**
+     * Constructor de la clase SumaDescendente.
+     *
+     * @param numero El número en cuestión.
+     */
     public SumaDescendente(long numero) {
         this.numero = numero;
     }
+    /**
+     * Calcula la suma descendente del número.
+     *
+     * @return La suma descendente.
+     */
     public long suma() {
         String num = String.valueOf(numero);
         long suma = 0;
