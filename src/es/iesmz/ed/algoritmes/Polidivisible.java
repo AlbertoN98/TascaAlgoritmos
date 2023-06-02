9d1089161960ae948f6a172e14773ec3920333b9
Alberto Nieto <anietor23@gmail.com>
2023-06-02 20:47:38 +0200

    numeros Polidivisibles Documentado.

--- src/es/iesmz/ed/algoritmes/Polidivisible.java
+++ src/es/iesmz/ed/algoritmes/Polidivisible.java
@@ -1,12 +1,23 @@
 package es.iesmz.ed.algoritmes;
-
+/**
+ * Esta clase representa representa un número polidivisible.
+ *@author: Alberto Nieto
+ *@version: 02/06/2023
+ */
 public class Polidivisible {
     private long numero;
+    /**
+     * Constructor de la clase Polidivisible.
+     * @param numero El número polidivisible.
+     */
 
     public Polidivisible(long numero) {
         this.numero = numero;
     }
-
+    /**
+     * Verifica si el número es divisible.
+     * @return true si el número es divisible, false en caso contrario.
+     */
     public boolean divisible() {
         if (numero < 10)
             return true;
@@ -21,6 +32,10 @@ public class Polidivisible {
         else
             return new Polidivisible(numero / 10).divisible();
     }
+    /**
+     * Verifica si el número es polidivisible.
+     * @return true si el número es polidivisible, false en caso contrario.
+     */
 
     public boolean polidivisibleCheck() {
         return divisible();
